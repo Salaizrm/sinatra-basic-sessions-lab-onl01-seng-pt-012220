986e538b38f1e509b73dbc2810e6ae2d873f1983
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-20 23:54:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,10 +14,7 @@ class App < Sinatra::Base
 
   post '/checkout' do
     @item = params[:item]
-    list = <<-LIST
-    Here is your shopping cart:
-    #{@item}
-    LIST
+    session["item"] = @item
 
     erb :checkout
   end
